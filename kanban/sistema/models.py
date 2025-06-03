77da9c0f50f5c8805dc72ccdb92e3320d329ee1e
Lucas Brembati Weizenmann <lucas8bitbw@gmail.com>
2025-06-02 20:19:02 -0400

    commit 02 "models e views quase la"

--- kanban/sistema/models.py
+++ kanban/sistema/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
-class Status(models.Model):
-    descricao = models.CharField(max_length=200)
+class Nivel(models.Model):
+    descricao = models.CharField(max_length=200, default="Nível")
+
+    def __str__(self):
+        return self.descricao
 
 class Tarefa(models.Model):
-    descricao = models.CharField(max_length=200)
-    Status = models.IntegerField(default=0)
+    descricao = models.CharField(max_length=200, blank=True)
+    titulo = models.CharField(max_length=200, default="Tarefa")
+    data = models.DateField(default=timezone.now)
+    nivel = models.ForeignKey(Nivel, on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return self.titulo
--- kanban/sistema/views.py
+++ kanban/sistema/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from .models import Tarefa
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Teste")
+    tarefas = Tarefa.objects.order_by('id')
+    contexto = {'tarefas':tarefas}
+    return render(request, 'paginas/index.html', contexto)
