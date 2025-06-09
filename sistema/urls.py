535976c83a9a49bc95e760bad712f97eb8f269b8
LucasBW-06 <lucas8bitbw@gmail.com>
2025-06-09 18:47:35 -0300

    commit 06 'adicionador for no html'

--- sistema/urls.py
+++ sistema/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'sistema'
+
 urlpatterns = [
     path('', views.index, name="index"),
+    path('<int:id_tarefa>/modificar_nivel/<str:modificador>', views.modificiar_nivel, name='modificar_nivel'),
 ]
--- sistema/views.py
+++ sistema/views.py
@@ -1,8 +1,22 @@
 from django.http import HttpResponse
 from .models import Tarefa
-from django.shortcuts import render
+from .models import Nivel
+from django.shortcuts import render, get_object_or_404, redirect
 
 def index(request):
-    tarefas = Tarefa.objects.order_by('id')
-    contexto = {'tarefas':tarefas}
+    tarefas = Tarefa.objects.order_by('titulo')
+    niveis = Nivel.objects.order_by('id')
+    nivel_min = Nivel.objects.order_by('id').first().id
+    nivel_max = Nivel.objects.order_by('-id').first().id
+    contexto = {'tarefas':tarefas,
+                'niveis':niveis,
+                'nivel_min': nivel_min,
+                'nivel_max': nivel_max}
     return render(request, 'paginas/index.html', contexto)
+
+def modificiar_nivel(request, id_tarefa, modificador):
+    tarefa = get_object_or_404(Tarefa, pk=id_tarefa)
+    tarefa.nivel = get_object_or_404(Nivel, id=(tarefa.nivel.id + int(modificador)))
+    tarefa.save()
+
+    return redirect('sistema:index')
